5e076db79e980a5b45777e33ef107fdd3839bfae
Mia Smith <smi22077@byui.edu>
2025-05-02 10:57:05 -0600

    Loops Assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1,100);
+
+        Console.Write("What is your guess? ");
+        string guessString = Console.ReadLine();
+        int guess = int.Parse(guessString);
+
+        while(guess != randomNumber)
+        {
+            if (guess < randomNumber)
+            {
+                Console.WriteLine("Higher");
+                Console.Write("What is your guess? ");
+                guessString = Console.ReadLine();
+                guess = int.Parse(guessString);
+            }
+            else if (guess > randomNumber)
+            {
+                Console.WriteLine("Lower");
+                Console.Write("What is your guess? ");
+                guessString = Console.ReadLine();
+                guess = int.Parse(guessString);
+            }
+        }
+
+        Console.WriteLine("You guessed it!");
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,8 +2,37 @@ using System;
 
 class Program
 {
+
+    static double AddNumbers(double a, double b)
+    {
+        return a + b;
+    }
+
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+    //     List<int> myNumbers = new List<int>();
+    //     myNumbers.Add(25);
+    //     myNumbers.Add(32);
+
+    //     foreach(int i in myNumbers)
+    //     {
+    //         Console.WriteLine(i);
+    //     }
+
+        double total = AddNumbers(123.98, 985.123456);
+        Console.WriteLine(total);
+
+        bool done = false;
+        while(!done)
+        {
+            //...
+            done = true;
+        }
+
+        do
+        {
+
+        }   while(!done);
     }
 }
