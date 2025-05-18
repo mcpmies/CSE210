7cebdc1f2c48a102af4bb991c4f552494edd2068
Mia Smith <smi22077@byui.edu>
2025-05-17 23:37:27 -0600

    update

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -2,8 +2,31 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    void Main(string[] args)
     {
         Console.WriteLine("Hello Develop02 World!");
+
+
+
+        switch(...)
+        {
+            case 1:
+                //...
+                break;
+            case 2:
+                //...
+                break;
+            case 3:
+                //...
+                break;
+            case 4:
+                //...
+                break;
+            case 5:
+                //...
+                break;
+            default:
+                //...
+        }
     }
 }
