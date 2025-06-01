e8bbcd747b82d67cd1d9f5da05bab22203ba62f8
Mia Smith <smi22077@byui.edu>
2025-05-31 21:16:12 -0600

    update

---
+++ WordCounter2/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        WordCounter wordCounter = new WordCounter("This is a test sentence to allow testing my new class");
+        wordCounter.DisplayWords();
+
+        int count = wordCounter.CountSingleWord("test");
+        Console.WriteLine(count);
+    }
+}
+
