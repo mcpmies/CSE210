8d0caf1e9c4cf10b2c34b2b516464edbf23d7b67
Mia Smith <mcpmies13@gmail.com>
2026-05-06 14:50:32 -0600

    Learning 02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Adobe";
+        job1._startYear = 2010;
+        job1._endYear = 2015;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Computer Scientist";
+        job2._company = "Microsoft";
+        job2._startYear = 2016;
+        job2._endYear = 2026;
+
+        Resume resume1 = new Resume();
+        resume1._name = "John Smith";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
     }
 }
