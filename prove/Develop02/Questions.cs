3ffdf64269805948e929af325b12e6aa690a7c89
Mia Smith <mcpmies13@gmail.com>
2026-05-14 11:55:33 -0600

    Journal Updates

---
+++ prove/Develop02/Questions.cs
@@ -0,0 +1,21 @@
+class Questions
+{
+    private List<string> _questionList = new List<string>()
+    {
+        "What was the best part of your day?",
+        "What was the worst part of your day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the most interesting thing that happened today?",
+        "What were you grateful for today?"
+    };
+    
+    private Random random = new Random();
+
+    public string SelectQuestion()
+    {
+        int index = random.Next(_questionList.Count);
+        string randomQuestion = _questionList[index];
+
+        return randomQuestion;
+    }
+}
