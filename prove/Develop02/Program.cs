65c9f6364b228a2d5e75ea426029af14c82ac3c6
Mia Smith <mcpmies13@gmail.com>
2026-05-21 16:53:37 -0600

    Comment?

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,9 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
+        // I added a little loading animation in the menu class that runs for a few seconds.
+        
         Menu menu = new Menu();
         menu.MenuSelect();
     }
 }
-
-// I added a little loading animation in the menu class that runs for a few seconds.
